3c76358a3e63fd440789875a12d7594aee589a66
ankitjnvynr <ankitbkana@outlook.com>
2024-12-30 14:46:46 +0530

    footer changes

--- src/components/footers/FooterOne.jsx
+++ src/components/footers/FooterOne.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import footerLogo from "../../assets/img/footer_logo.svg";
+import footerLogo from "../../assets/img/logo.png";
 
 export const FooterOne = () => {
   return (
@@ -12,20 +12,16 @@ export const FooterOne = () => {
             <div className="td_footer_widget">
               <div className="td_footer_text_widget td_fs_18">
                 <img src={footerLogo} alt="Logo" />
-                <p>
-                  Far far away, behind the word mountains, far from the
-                  Consonantia, there live the blind texts.
-                </p>
+                <p>"Empowering Minds, Shaping Futures."</p>
               </div>
               <ul className="td_footer_address_widget td_medium td_mp_0">
                 <li>
                   <i className="fa-solid fa-phone-volume"></i>
-                  <a href="cal:+23(000)68603">+23 (000) 68 603</a>
+                  <a href="#"> 01744-225803 , 225803</a>
                 </li>
                 <li>
-                  <i className="fa-solid fa-location-dot"></i>66 broklyn golden
-                  street <br />
-                  600 New york. USA
+                  <i className="fa-solid fa-location-dot"></i>Shri Krishna Ayush
+                  University,Umri Road, Sector 8,Kurukshetra, Haryana – 136118
                 </li>
               </ul>
             </div>
@@ -40,19 +36,17 @@ export const FooterOne = () => {
                   <Link to="/">Home</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/">About</Link>
                 </li>
                 <li>
-                  <Link to="/contact">Contact</Link>
+                  <Link to="/">Contact</Link>
+                </li>
+
+                <li>
+                  <Link to="/">Help Center</Link>
                 </li>
                 <li>
-                  <Link to="/refund">Refund</Link>
-                </li>
-                <li>
-                  <Link to="/help">Help Center</Link>
-                </li>
-                <li>
-                  <Link to="/privacy">Privacy Policy</Link>
+                  <Link to="/">Privacy Policy</Link>
                 </li>
               </ul>
             </div>
@@ -64,22 +58,28 @@ export const FooterOne = () => {
               </h2>
               <ul className="td_footer_widget_menu">
                 <li>
-                  <Link to="/course-details">Business Coach</Link>
+                  <Link to="/">BAMS</Link>
                 </li>
                 <li>
-                  <Link to="/course-details">Development Coach</Link>
+                  <Link to="/">Diploma in Panchakarma</Link>
                 </li>
                 <li>
-                  <Link to="/course-details">Testimonials</Link>
+                  <Link to="/">
+                    Certificate in Herbal Medicine
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/course-details">Seo Optimization</Link>
+                  <Link to="/">
+                    Master in Ayurvedic Pharmacology
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/course-details">Web design</Link>
+                  <Link to="/">
+                    Ayurvedic Nutrition and Dietetics
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/course-details">Life Coach</Link>
+                  <Link to="/">PhD in Ayurveda</Link>
                 </li>
               </ul>
             </div>
@@ -91,8 +91,7 @@ export const FooterOne = () => {
               </h2>
               <div className="td_newsletter td_style_1">
                 <p className="td_mb_20 td_opacity_7">
-                  Far far away, behind the word mountains, far from the
-                  Consonantia.
+                  subscribe to our newsletter to get the latest news and updates
                 </p>
                 <form action="#" className="td_newsletter_form">
                   <input
@@ -121,7 +120,7 @@ export const FooterOne = () => {
                   <i className="fa-brands fa-instagram"></i>
                 </a>
                 <a href="#" className="td_center">
-                  <i className="fa-brands fa-pinterest-p"></i>
+                  <i className="fa-brands fa-youtube"></i>
                 </a>
               </div>
             </div>
@@ -132,14 +131,14 @@ export const FooterOne = () => {
         <div className="container">
           <div className="td_footer_bottom_in">
             <p className="td_copyright mb-0">
-              Copyright ©educve | All Right Reserved
+              Copyright © BigByte Innovations Pvt. Ltd. | All Right Reserved
             </p>
             <ul className="td_footer_widget_menu">
               <li>
-                <Link to="/terms">Terms & Conditions</Link>
+                <Link to="/">Terms & Conditions</Link>
               </li>
               <li>
-                <Link to="/privacy">Privacy & Policy</Link>
+                <Link to="/">Privacy & Policy</Link>
               </li>
             </ul>
           </div>
